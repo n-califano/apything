c415984cf8a615a3f86e78193092ecf24729ad0b
Niccolò Califano <n.califano@eldes.it>
2025-03-31 21:35:51 +0200

    added upload endpoint to config.yml and refactored Document class to use it

--- src/apything/endpoints/document.py
+++ src/apything/endpoints/document.py
@@ -1,13 +1,13 @@
 class Document:
     def __init__(self, client):
         self.client = client  # Reference to APIClient
-        self.url = f"{self.client.base_url}/document"
+        self.endpoints = self.client.config['endpoints']['documents']
 
     def upload_file(self, file_path):
         file_to_upload = {
             "file": open(file_path, "rb")
         }
-        upload_url = f"{self.url}/upload"
+        upload_url = f"{self.client.base_url}/{self.endpoints['upload']}"
         response = self.client.session.post(upload_url, headers=self.client.session.headers, files=file_to_upload)
         
         return response.json()
