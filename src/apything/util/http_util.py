e819f28586a33ed68641a5aa60bcc3972dfe8391
Niccolò Califano <n.califano@eldes.it>
2025-04-05 00:17:47 +0200

    added HttpUtil class and its safe_request method to encapsulate all the exception handling of HTTP requests

---
+++ src/apything/util/http_util.py
@@ -0,0 +1,22 @@
+from requests.exceptions import RequestException
+
+class ApythingException(Exception):
+    pass
+
+class HttpUtil:
+    @staticmethod
+    def safe_request(session, url, headers, method='GET', data=None):
+        try:
+            response = session.request(method, url, json=data, headers=headers)
+        except RequestException as e:
+            # Handle network or HTTP request errors
+            raise ApythingException("Error: request failed") from e
+        
+        if response.ok:
+            try:
+                return response.json()
+            except ValueError:
+                # Some endpoints do not return any json data, only HTTP codes
+                return response.ok
+        else:
+            raise ApythingException(f"Error: request returned {response.status_code} code\nResponse: {response.text}")
