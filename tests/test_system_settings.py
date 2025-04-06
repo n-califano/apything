55d25ca5f08b9b1a1a25dfc71734599d98e7905d
Niccolò Califano <n.califano@eldes.it>
2025-04-06 21:21:28 +0200

    issue #1 - refactored test_remove_documents

--- tests/test_system_settings.py
+++ tests/test_system_settings.py
@@ -3,7 +3,6 @@ def test_remove_documents(api_client, tmp_files):
     _, _, files = api_client.documents.upload_files(tmp_files)
     docs_to_remove = [file.location for file in files]
 
-    json = api_client.system_settings.remove_documents(docs_to_remove)
+    is_success = api_client.system_settings.remove_documents(docs_to_remove)
     
-    assert json['success'] is True
-    assert json['message'] == "Documents removed successfully"
+    assert is_success is True
