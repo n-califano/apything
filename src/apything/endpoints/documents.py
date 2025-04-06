82f3d911fb219e7dc290f606894df542dcf02acd
Niccolò Califano <n.califano@eldes.it>
2025-04-06 13:50:17 +0200

    refactored documents endpoints to use safe_request method from httputil and return model objects instead of plain json

--- src/apything/endpoints/documents.py
+++ src/apything/endpoints/documents.py
@@ -1,3 +1,6 @@
+from ..models.documents_model import Document
+from ..util.http_util import HttpUtil
+
 class Documents:
     def __init__(self, client):
         self.client = client  # Reference to APIClient
@@ -11,15 +14,29 @@ class Documents:
             "file": open(file_path, "rb")
         }
         upload_url = f"{self.base_url}/{self.endpoints['upload']}"
-        response = self.session.post(upload_url, headers=self.headers, files=file_to_upload)
-        
-        return response.json()
+        json_data = HttpUtil.safe_request(self.session, upload_url, self.headers, method='POST', files=file_to_upload)
+        doc = Document.from_json(json_data['documents'][0])
+
+        return (json_data['success'] is True, json_data['error'], doc)
     
     def upload_files(self, file_paths):
-        return [self.upload_file(file_path) for file_path in file_paths]
+        upload_responses = [self.upload_file(file_path) for file_path in file_paths]
+        
+        errors = []
+        docs = []
+        all_success = True
+        for success, error, doc in upload_responses:
+            docs.append(doc)
+            if not success:
+                all_success = False
+                errors.append(error)
+
+        return all_success, errors, docs
     
     def get_documents(self):
         docs_url = f"{self.base_url}/{self.endpoints['documents']}"
-        response = self.session.get(docs_url, headers=self.headers)
-
-        return response.json()
+        json_data = HttpUtil.safe_request(self.session, docs_url, self.headers, method='GET')
+        folders = json_data['localFiles']['items']
+        docs = [Document.from_json(docItem, folder['name']) for folder in folders for docItem in folder['items']]
+        
+        return docs
