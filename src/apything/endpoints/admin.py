deacca8c5282174ca4c8e1ad10c41289dbbce1b4
Niccolò Califano <n.califano@eldes.it>
2025-04-07 22:04:48 +0200

    issue #3 and issue #4

--- src/apything/endpoints/admin.py
+++ src/apything/endpoints/admin.py
@@ -1,6 +1,7 @@
 from ..models.admin_model import User
 from ..util.http_util import HttpUtil
 
+
 class Admin:
     def __init__(self, client):
         self.client = client  # Reference to APIClient
@@ -9,8 +10,30 @@ class Admin:
         self.base_url = self.client.base_url
         self.headers = self.client.session.headers
 
-    def get_users(self):
+
+    def get_users(self) -> list:
         users_url = f"{self.base_url}/{self.endpoints['users']}"
         json_data = HttpUtil.safe_request(self.session, users_url, self.headers, method='GET')
 
         return [User.from_json(json_item) for json_item in json_data['users']]
+    
+
+    def create_user(self, username: str, password: str, role: str) -> int:
+        user_to_add = {
+            "username": username,
+            "password": password,
+            "role": role
+        }
+
+        create_url = f"{self.base_url}/{self.endpoints['new']}"
+        json_data = HttpUtil.safe_request(self.session, create_url, self.headers, method='POST', data=user_to_add)
+
+        return json_data['user']['id']
+    
+
+    def remove_user(self, user_id: int) -> bool:
+        endpoint = self.endpoints['delete'].format(id=user_id)
+        delete_url = f"{self.base_url}/{endpoint}"
+        json_data = HttpUtil.safe_request(self.session, delete_url, self.headers, method='DELETE')
+
+        return json_data['success'] is True and json_data['error'] is None
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,15 +1,49 @@
 def test_get_users(api_client):
     # Setup
-    #TODO: create a user
-
+    user_id = api_client.admin.create_user(username="test_user", password="p4ssw0rd", role='default')
+    
     users = api_client.admin.get_users()
     
     assert len(users) != 0
-    #TODO: add asserts on ad hoc created user
+    assert any(user.username == "test_user" and user.role == 'default' for user in users)
 
     # Teardown
-    #TODO: remove user
+    api_client.admin.remove_user(user_id=user_id)
 
-    
+def test_create_user(api_client):
+    user_id = api_client.admin.create_user(username="test_user", password="p4ssw0rd", role='default')
+    users = api_client.admin.get_users()
 
-    
+    assert isinstance(user_id, int)
+    assert user_id > 0
+    assert any(user.username == "test_user" and user.role == 'default' for user in users)
+
+    manager_id = api_client.admin.create_user(username="test_manager", password="m4n4g3r!!", role='manager')
+    users = api_client.admin.get_users()
+
+    assert isinstance(manager_id, int)
+    assert manager_id > 0
+    assert any(user.username == "test_manager" and user.role == 'manager' for user in users)
+
+    admin_id = api_client.admin.create_user(username="test_admin", password="adminadmin", role='admin')
+    users = api_client.admin.get_users()
+
+    assert isinstance(admin_id, int)
+    assert admin_id > 0
+    assert any(user.username == "test_admin" and user.role == 'admin' for user in users)
+
+    # Teardown
+    api_client.admin.remove_user(user_id=user_id)
+    api_client.admin.remove_user(user_id=manager_id)
+    api_client.admin.remove_user(user_id=admin_id)
+
+
+def test_remove_user(api_client):
+    # Setup
+    user_id = api_client.admin.create_user(username="test_user", password="p4ssw0rd", role='default')
+
+    is_success = api_client.admin.remove_user(user_id=user_id)
+    users = api_client.admin.get_users()
+
+    assert is_success is True
+    assert not any(user.username == "test_user" and user.role == 'default' for user in users)
