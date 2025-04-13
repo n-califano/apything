25e2d78b4910325c4df192db283759bc5deef0b6
Niccolò Califano <n.califano@eldes.it>
2025-04-13 19:23:29 +0200

    issue #10 - added tests for chat_with_workspace wrapper method

--- tests/test_workspaces.py
+++ tests/test_workspaces.py
@@ -1,4 +1,13 @@
-from apything import WorkspaceRequest
+import pytest
+import string
+import secrets
+from apything import WorkspaceRequest, ChatRequest, WorkspaceResponse, Attachment, ApythingRequestException
+
+
+@pytest.fixture
+def random_session_id(length=16):
+    characters = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(characters) for _ in range(length))
 
 
 def test_update_embeddings(api_client, tmp_files, test_workspace):
@@ -44,3 +53,71 @@ def test_delete_workspace(api_client):
 
     is_success = api_client.workspaces.delete_workspace("test-delete-workspace")
     assert is_success is True
+
+
+def test_chat_with_workspace_no_attachments_success(api_client, random_session_id: str, test_workspace: WorkspaceResponse):
+    request = ChatRequest(message="My hair is blue", mode="chat", sessionId=random_session_id)
+    response = api_client.workspaces.chat_with_workspace(workspace_slug=test_workspace.slug, request=request)
+
+    assert response.id is not None and response.id != ""
+    assert response.type == "textResponse"
+    assert response.textResponse is not None and response.textResponse != ""
+    assert response.error is None
+
+    request = ChatRequest(message="Do you remember what colors my hair is?", mode="chat", sessionId=random_session_id)
+    response = api_client.workspaces.chat_with_workspace(workspace_slug=test_workspace.slug, request=request)
+
+    assert response.id is not None and response.id != ""
+    assert response.type == "textResponse"
+    # Here we check that the session actually persisted (same sessionId) and the model is able to give us 
+    # information based on our previous request
+    assert "blue" in response.textResponse.lower()
+    assert response.error is None
+
+
+# WARNING: depending on the LLM used you could get an error. I tested it with the Gemini API and to make it work
+# I had to set the Safety Setting in the LLM Preferences to 'None'. With the default setting ('Block some') i kept
+# getting a 'Candidate was blocked due to SAFETY' error.
+def test_chat_with_workspace_with_attachments_success(api_client, random_session_id: str, test_workspace: WorkspaceResponse):
+    with open("assets/art.b64", 'r') as file:
+        base64_uri = file.read()
+    
+    attachment = Attachment(name="art.webp", mime="image/webp", contentString=base64_uri)
+    request = ChatRequest(message="What's in the attached file?", mode="chat", sessionId=random_session_id, attachments=[attachment])
+    response = api_client.workspaces.chat_with_workspace(workspace_slug=test_workspace.slug, request=request)
+    text = response.textResponse.lower()
+        
+    assert response.id is not None and response.id != ""
+    assert response.type == "textResponse"
+    assert "art" in text or "clown" in text
+    assert response.error is None
+
+
+def test_chat_with_workspace_query_mode_success(api_client, random_session_id: str, test_workspace):
+    # Setup
+    _, _, doc = api_client.documents.upload_file(file_path="assets/nytheris.txt")
+    is_success = api_client.workspaces.update_embeddings(test_workspace.slug, [doc.location])
+    if not is_success:
+        raise Exception("The embedding of the file failed.")
+    
+    request = ChatRequest(message="What are the rumors about Nytheris Prime?", mode="query", sessionId=random_session_id)
+    response = api_client.workspaces.chat_with_workspace(workspace_slug=test_workspace.slug, request=request)
+    text = response.textResponse.lower()
+
+    assert response.id is not None and response.id != ""
+    assert response.type == "textResponse"
+    assert "sentient weather systems" in text and "time fractures" in text
+    assert response.error is None
+    
+    # Teardown
+    api_client.system_settings.remove_documents([doc.location])
+
+
+def test_chat_with_workspace_unsupported_mode_failure(api_client, random_session_id: str, test_workspace):
+    request = ChatRequest(message="Hello there", mode="unsupported-mode", sessionId=random_session_id)
+    
+    with pytest.raises(ApythingRequestException) as ex:
+        api_client.workspaces.chat_with_workspace(workspace_slug=test_workspace.slug, request=request)
+
+    expected_exception_msg = 'Error 400: unsupported-mode is not a valid mode.'
+    assert expected_exception_msg in str(ex.value)
