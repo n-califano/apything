fc27f966b236abd02f4aa01c0802f7c256441d86
Niccolò Califano <n.califano@eldes.it>
2025-04-13 19:18:16 +0200

    issue #10 - added Attachment, ChatRequest, ChatSource, ChatMetrics, ChatResponse classes to workspaces_model.py

--- src/apything/models/workspaces_model.py
+++ src/apything/models/workspaces_model.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Literal, Optional
 
 
 @dataclass
@@ -26,6 +27,52 @@ class WorkspaceResponse(WorkspaceRequest):
     agentProvider: str
     agentModel: str
     vectorSearchMode: str
+
+    @classmethod
+    def from_json(cls, json_data: dict):
+        return cls(**json_data)
+    
+
+@dataclass
+class Attachment():
+    name: str
+    mime: str
+    contentString: str
+
+
+@dataclass
+class ChatRequest():
+    message: str
+    mode: Literal["query", "chat"]
+    sessionId: str
+    attachments: List[Attachment] = field(default_factory=list)
+
+
+@dataclass
+class ChatSource():
+    title: str
+    chunk: str
+
+
+@dataclass
+class ChatMetrics():
+    prompt_tokens: int 
+    completion_tokens: int 
+    total_tokens: int 
+    outputTps: float 
+    duration: float
+
+
+@dataclass
+class ChatResponse():
+    id: str
+    type: Literal["abort", "textResponse"]
+    textResponse: str
+    sources: List[ChatSource]
+    close: bool
+    error: Optional[str]  # Can be None or a string describing the failure
+    metrics: ChatMetrics
+    chatId: int = None  # This property has a value only if using 'chat' mode, is None for 'query' mode
 
     @classmethod
     def from_json(cls, json_data: dict):
